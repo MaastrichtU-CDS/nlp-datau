eec86249b7e338141a5e97bcd63e864db4bfc876
Sander Puts <putssander@gmail.com>
2020-03-06 15:19:39 +0100

    merge excel documents

---
+++ nlp_datau/index_to_xlsx.py
@@ -0,0 +1,46 @@
+import json
+
+from elasticsearch import Elasticsearch
+from nlp_datau.data_utils import DataUtils
+# import spacy
+import logging
+
+from spacy.matcher import PhraseMatcher
+logging.basicConfig()
+logger = logging.getLogger('index_to_xlsx')
+logger.setLevel(level=logging.INFO)
+
+es = Elasticsearch()
+
+
+
+
+class IndexToXlsx(object):
+
+    def __init__(self, es_index):
+        self.es_index = es_index
+        # self.nlp = spacy.load(model)
+
+    def get_source(self, doc_id):
+        try:
+            return self.get_doc(doc_id)["_source"]
+        except Exception as e:
+            logger.error(e)
+        return None
+
+    def get_source_field(self, source, key):
+        try:
+            return DataUtils.get_json_field_dot_notation(key, source)
+        except Exception as e:
+            logger.error(e)
+        return None
+
+    def get_doc(self, doc_id):
+        return es.get(index=self.es_index, id=doc_id)
+
+    def merge(self, df, df_id_column, field_map):
+        df['source'] = df.apply(lambda row: self.get_source(row[df_id_column]), axis=1)
+        for key in field_map:
+            df[key] = df.apply(lambda row: self.get_source_field(row['source'], field_map[key]), axis=1)
+        df = df.drop(columns=['source'])
+        return df
--- nlp_datau/tests/es_index_write_tests.py
+++ nlp_datau/tests/es_index_write_tests.py
@@ -1,10 +1,13 @@
 import unittest
+import pandas
 
+from nlp_datau import index_to_xlsx
 from nlp_datau.index_to_json import IndexToJson
+from nlp_datau.index_to_xlsx import IndexToXlsx
 from nlp_datau.xml_to_index import XmlToIndex
 
 # elasticsearch index (start elasticsearch with docker-compose)
-es_index = "xml-index"
+es_index = "xml-mumc-index"
 
 
 class TestIndexMethods(unittest.TestCase):
@@ -12,12 +15,49 @@ class TestIndexMethods(unittest.TestCase):
     # convert xml documents to json and index in elasticsearch
     def test_xml_to_index(self):
         # download MEANTIME corpus here: http://www.newsreader-project.eu/results/data/wikinews/
-        directory_in_str = "/Users/sanderputs/Data/Corpora/MEANTIME/meantime_newsreader_english_raw_NAF/corpus_gm"
-        extension = ".naf"
+        # directory_in_str = "/Users/sanderputs/Data/Corpora/MEANTIME/meantime_newsreader_english_raw_NAF/corpus_gm"
+        directory_in_str = '/Users/sanderputs/Data/Research/XML-data'
+        extension = ".xml"
         # xml configuration using json dot notation
-        field_id = "NAF.nafHeader.public.@publicId"
+        field_id = "ClinicalDocument.setId.@extension"
         # index directory
         XmlToIndex().iterate_xml_dir(directory_in_str, extension=extension, es_index=es_index, field_id=field_id)
+
+        # convert xml documents to json and index in elasticsearch
+    def test_mumc_xml_to_index(self):
+        # download MEANTIME corpus here: http://www.newsreader-project.eu/results/data/wikinews/
+        # directory_in_str = "/Users/sanderputs/Data/Corpora/MEANTIME/meantime_newsreader_english_raw_NAF/corpus_gm"
+        directory_in_str = '/Users/sanderputs/Data/Research/Longembolie28-02-2020'
+        extension = ".xml"
+        # xml configuration using json dot notation
+        field_id = "ClinicalDocument.setId.@extension"
+
+        mumc_whitelist_dict = {
+            "text": {
+                "field": "ClinicalDocument.component.structuredBody.component.section.entryRelationship.observationMedia.value.#text",
+                "type": "base64"
+            }
+        }
+        mumc_whitelist_dict = {
+            "text": {
+                "field": "ClinicalDocument.component.structuredBody.component.section.text",
+                "type": "xml",
+                "xml-field": "getReportTextByAccessionnrResult.REPORT.REPORT_BODY.TEXT",
+                "start-line": "Verslagstatus:",
+                "stop-line": "Met collegiale groet,"
+            }
+        }
+
+
+        # index directory
+        XmlToIndex().iterate_xml_dir(
+            directory_in_str,
+            extension=extension,
+            es_index=es_index,
+            field_id=field_id,
+            whitelist_dict=mumc_whitelist_dict)
+
+
 
     # write snippets of matching documents into doccano json format for annotation
     def test_index_to_doccano_json(self):
@@ -32,6 +72,22 @@ class TestIndexMethods(unittest.TestCase):
 
         pattern_terms = ["Ford"]
         index_to_json.doccano_snippet_training_out(docs, pattern_terms, directory_out_str)
+
+    # merge excel docs
+    def test_merge_excel(self):
+
+        id_column = "Accessionnr"
+        source_xlsx = "/Users/sanderputs/Data/Research/Longembolie/Longembolie - tekstsearch.xlsx"
+        result_xlsx = "/Users/sanderputs/Data/Research/Longembolie/Longembolie - tekstsearch - result.xlsx"
+
+        field_map = {"text": "text"}
+
+        df_input = pandas.read_excel(source_xlsx, header=0)
+        to_xlsx = IndexToXlsx(es_index)
+        result_df = to_xlsx.merge(df_input, id_column, field_map=field_map)
+        result_df.to_excel(result_xlsx)
+
+
 
 
 if __name__ == '__main__':
