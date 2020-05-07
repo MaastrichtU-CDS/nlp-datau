1f554f63cf0bbe11bbcb30fb91bcfd12f5574749
SanderPuts <putssander@gmail.com>
2020-05-07 11:19:33 +0200

    added column for conclusion and boolean for missing

--- nlp_datau/string_utils.py
+++ nlp_datau/string_utils.py
@@ -5,10 +5,15 @@ def select_regex(txt, regex=r"Conclusie:(.*?)DISCLAIMER"):
     if not txt:
         return None
     txt = str(txt).replace("\n", "").replace("\t", "")
-    item = None
     list = []
     for item in re.finditer(regex, txt):
         list.append(txt[item.start(): item.end()])
-    if item is None:
-        return "NOT_FOUND " + str(regex)
     return "\n".join(list)
+
+
+def regex_missing(txt, regex=r"Conclusie:(.*?)DISCLAIMER"):
+    if not txt:
+        return True
+    if len(re.finditer(regex, txt)) == 0:
+        return True
+    return ""
